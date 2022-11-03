cca6cce06de08e5259080ba2eb9a03f688655acf
Patrick Schilf <pschilf@unb.ca>
2022-11-03 13:30:31 -0300

    Enable custom plugin selectors

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,17 +1,34 @@
-Object.values(Cypress.config().plugins).forEach(plugin => {
-  const { commands } = require(`./plugins/${plugin.relativePath}`)
+const { commands, selectors } = Object
+  .values(Cypress.config().plugins)
+  .reduce(({commands, selectors}, plugin) => {
+    const { commands: pCommands, selectors: pSelectors } = require(`./plugins/${plugin.relativePath}`)
+    return {
+      commands: { ...commands, ...pCommands },
+      selectors: { ...selectors, ...pSelectors },
+    }}, {commands: {}, selectors: {}})
 
-  Object.entries(commands)
-    .forEach(([name, { fn, method = 'add', type = 'parent', subject = false }]) => {
-      if (method === 'add') {
-        Cypress.Commands.add(name, {
-          prevSubject: type === 'parent'
-            ? false
-            : subject,
-        }, fn)
-      }
-      else {
-        Cypress.Commands.overwrite(name, fn)
-      }
-    })
+Object.entries(commands)
+  .forEach(([name, { fn, method = 'add', type = 'parent', subject = false }]) => {
+    if (method === 'add') {
+      Cypress.Commands.add(name, {
+        prevSubject: type === 'parent'
+          ? false
+          : subject,
+      }, fn)
+    }
+    else {
+      Cypress.Commands.overwrite(name, fn)
+    }
+  })
+
+Cypress.Commands.overwrite('get', (originalFn, selector, options) => {
+  // Match shortcuts such as "widget:input:title" to get the form input element with the name "title"
+  if (selector.match(/^[a-zA-Z-_]+(\:([0-9a-zA-Z-_])+)+$/)) {
+    const base = selector.substring(0, selector.lastIndexOf(':'))
+    const name = selector.substring(selector.lastIndexOf(':') + 1)
+    if (selectors.hasOwnProperty(base)) {
+      return originalFn(selectors[base](name), options)
+    }
+  }
+  return originalFn(selector, options)
 })
